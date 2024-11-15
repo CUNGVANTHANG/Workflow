e0baf87751399cfe54e455593057f8918e61e12a
CUNGVANTHANG <cungvanthang2k3@gmail.com>
2024-11-15 13:38:36 +0700

    react-jsonschema-form

--- technology/react-jsonschema-form/README.md
+++ technology/react-jsonschema-form/README.md
@@ -3,3 +3,8 @@
 ```
 npm install @rjsf/core @rjsf/utils @rjsf/validator-ajv8 --save
 ```
+
+## Using
+
+Dùng để validate form
+
--- technology/react-jsonschema-form/src/App.tsx
+++ technology/react-jsonschema-form/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Form from '@rjsf/core';
+import { RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv8';
+import './App.css';
+
+const schema: RJSFSchema = {
+  title: 'Test form',
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+    },
+    age: {
+      type: 'number',
+    },
+  },
+};
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='container'>
+      <h1>Test Form</h1>
+      <Form
+        schema={schema}
+        validator={validator}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
